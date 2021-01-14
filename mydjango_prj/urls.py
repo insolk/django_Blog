ca3518fbd41f3738d9ef80e12b3b4ac5bf1c50c9
insolk <rkddlsthf123@naver.com>
2021-01-14 15:11:39 +0900

    add login

--- mydjango_prj/urls.py
+++ mydjango_prj/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('accounts/login/', auth_view.LoginView.as_view(template_name='registration/login.html'), name='login'),
 ]
